36465f3133be0195ea7d7efe3fc1047c8772c563
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-03-28 00:53:50 -0600

    Create project

---
+++ backend/src/test/kotlin/com/getyourtours/GetYourToursApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.getyourtours
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GetYourToursApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
