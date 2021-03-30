43baff29f01adadce4030aa36a3e3c2a6c09e002
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-03-30 04:21:31 -0600

    add Test for get All countries

--- backend/src/test/java/com/getyourtour/GetYourTourApplicationTests.java
+++ backend/src/test/java/com/getyourtour/GetYourTourApplicationTests.java
@@ -1,13 +1,25 @@
 package com.getyourtour;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.getyourtour.Country;
+
 
 @SpringBootTest
 class GetYourTourApplicationTests {
 
+	private DaoModel db = new DaoModel();
+
 	@Test
-	void contextLoads() {
+	void GetAllCountries(){
+		List<Country> list = db.getAllCountries();
+		assertEquals(list.get(0).getName(), "Costa Rica");
+		assertEquals(list.get(1).getName(), "Mexico");
 	}
 
 }
