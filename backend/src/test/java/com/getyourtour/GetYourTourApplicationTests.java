a474a69c8a032af50e1bee70da4b606c4f023a9e
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-03-30 00:10:53 -0600

    add java classes

---
+++ backend/src/test/java/com/getyourtour/GetYourTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getyourtour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetYourTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
