76b82d88f0777a3f2da60826196ecc71e455fac8
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-03-30 04:21:58 -0600

    Create GetYourTourApplication.java

---
+++ backend/src/main/java/com/getyourtour/controller/GetYourTourApplication.java
@@ -0,0 +1,13 @@
+package com.getyourtour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetYourTourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetYourTourApplication.class, args);
+    }
+
+}
