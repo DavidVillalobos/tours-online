2c1c3318eda2c8f2df7e7da3e8ff26754f874f90
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-03-30 23:27:37 -0600

    update sql

---
+++ backend/database/Data.sql
@@ -0,0 +1,7 @@
+-- File: Data.sql
+-- Date: 2021/03/30
+-- Author: Luis Villalobos
+
+-- Insert of Countries
+insert into Country(Name) values ('Costa Rica');
+insert into Country(Name) values ('Mexico');
--- backend/database/DropModel.sql
+++ backend/database/DropModel.sql
@@ -1,3 +1,7 @@
+-- File: DropModel.sql
+-- Date: 2021/03/30
+-- Author: Luis Villalobos
+
 USE [master]
 GO
 DROP DATABASE [GYTModel]
