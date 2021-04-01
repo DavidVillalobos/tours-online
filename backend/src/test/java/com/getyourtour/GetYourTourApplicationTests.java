8ae50136b40521f8c0bb46ba4f70c651fbdb699e
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-04-01 00:45:02 -0600

    Update GetYourTourApplicationTests.java
    change test

--- backend/src/test/java/com/getyourtour/GetYourTourApplicationTests.java
+++ backend/src/test/java/com/getyourtour/GetYourTourApplicationTests.java
@@ -1,3 +1,8 @@
+/*
+ * File: GetYourTourApplicationTests.java
+ * author: David Villalobos
+ * Date: 2021/03/31
+ */
 package com.getyourtour;
 
 import org.junit.jupiter.api.Test;
@@ -7,8 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.getyourtour.Country;
-
 
 @SpringBootTest
 class GetYourTourApplicationTests {
@@ -16,8 +19,11 @@ class GetYourTourApplicationTests {
 	private DaoModel db = new DaoModel();
 
 	@Test
-	void GetAllCountries(){
+	void TestCountry(){
 		List<Country> list = db.getAllCountries();
+		// if(list.size() == 0 ){
+			// add data
+		// }
 		// assertEquals(list.get(0).getName(), "Costa Rica");
 		// assertEquals(list.get(1).getName(), "Mexico");
 	}
