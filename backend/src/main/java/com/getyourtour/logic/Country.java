73a8851eef820436ce94ea0704c6fc379d02b020
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-04-01 05:01:58 -0600

    Update Country.java

--- backend/src/main/java/com/getyourtour/logic/Country.java
+++ backend/src/main/java/com/getyourtour/logic/Country.java
@@ -1,30 +1,40 @@
 /*
  * File: Country.java
  * author: David Villalobos
- * Date: 2021/03/31
+ * Date: 2021/04/01
  */
+
 package com.getyourtour.logic;
 
 public class Country{
-    
-    private int Id;
-    private String Name;
 
-    public Country(int Id, String Name){
-        this.Id = Id;
-        this.Name = Name;
-    }
-    
-    public int getId(){
-        return Id;
+    private Integer id;
+    private String name;
+
+    public Country(){
+        this.id = 0;
+        this.name = "";
     }
 
-    public String getName(){
-        return Name;
+    public Country(Integer id, String name){
+        this.id = id;
+        this.name = name;
     }
 
-    public void setName(String Name){
-        this.Name = Name;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
 }
