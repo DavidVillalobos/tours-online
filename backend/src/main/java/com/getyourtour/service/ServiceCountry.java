8dbf793be1cc94a5a9e0c05725600c0a8f72a428
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-04-02 00:02:32 -0600

    Create ServiceCountry.java

---
+++ backend/src/main/java/com/getyourtour/service/ServiceCountry.java
@@ -0,0 +1,35 @@
+package com.getyourtour.service;
+
+import com.getyourtour.dao.DaoCountry;
+import com.getyourtour.model.Country;
+
+import java.util.List;
+
+public class ServiceCountry {
+
+    private DaoCountry dao_country = new DaoCountry();
+
+    public Country getCountry(Integer id){
+        return dao_country.get(id);
+    }
+
+    public List<Country> getAllCountries(){
+        return dao_country.get();
+    }
+
+    public int addCountry(Country country){
+        if(country.getName().isEmpty()){
+            return -1;
+        }
+        return dao_country.post(country);
+    }
+
+    public int updateCountry(Country country){
+        return dao_country.put(country);
+    }
+
+    public int deleteCountry(Integer id){
+        return dao_country.delete(id);
+    }
+
+}
