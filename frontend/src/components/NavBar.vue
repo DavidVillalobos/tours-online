185df37558316054e8a8d7c6dd34047d08dedac9
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-04-03 05:21:06 -0600

    Add Frontend login

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -26,11 +26,33 @@
       </template>
       <template v-else>
       <b-navbar-nav class="ml-auto">
-        <b-nav-item href="#">Iniciar Sesion</b-nav-item>
+        <b-button variant="dark" v-b-modal.modal-login>
+          <b-icon-person-fill variant="light"></b-icon-person-fill>
+          Iniciar Sesion
+        </b-button>
+        <b-button variant="dark" v-b-modal.modal-register>
+          Registrarse
+        </b-button>
       </b-navbar-nav>
-      <b-avatar variant="warning"></b-avatar>
       </template>
     </b-navbar>
+    <b-modal id="modal-login" hide-header
+      @show="resetModal" @hidden="resetModal" @ok="login"
+      cancel-title="Cancelar" ok-title="Iniciar"
+    >
+    <div class="d-block text-center">
+      <h3>
+        <b-icon-person-fill variant="dark"></b-icon-person-fill>
+        Iniciar Sesion
+      </h3>
+    </div>
+    <b-form-group label-cols="4" label-cols-lg="2" label="Email">
+      <b-form-input v-model="email"></b-form-input>
+    </b-form-group>
+    <b-form-group label-cols="4" label-cols-lg="2" label="contraseña">
+      <b-form-input v-model="password"></b-form-input>
+    </b-form-group>
+    </b-modal>
   </div>
 </template>
 
@@ -38,22 +60,29 @@
 export default {
   name: 'Navbar',
   props: {
-    user: String
+    user: String,
+    email: String,
+    password: String,
   },
   methods: {
-    avatarName: function(){
+    avatarName() {
       var result = ''
       for(let word of this.user.split(' ')){
         result += word[0];
       }
       return result;
+    },
+    resetModal(){
+      this.email = ''
+      this.password = ''
+    },
+    login(){
+      alert("Login email: " + this.email + "  password: " + this.password)
     }
   }
 }
 
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
