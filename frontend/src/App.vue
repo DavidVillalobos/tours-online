f609d4e3347e65adb6c278c32fbdede4d9d572e3
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-04-03 03:17:11 -0600

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Navbar user=""/>
+    <SearchTour user="" id="search"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/NavBar'
+import SearchTour from './components/SearchTour'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    SearchTour
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+#search{
+  margin: 3% 5% 0% 5%;
 }
 </style>
