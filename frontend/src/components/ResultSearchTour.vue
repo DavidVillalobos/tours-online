537a244a29453feed84312682734129013ff60ac
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-04-03 05:21:11 -0600

    Create ResultSearchTour.vue

---
+++ frontend/src/components/ResultSearchTour.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <b-row v-for="(tour, idx) in tours" :key="idx">
+      <b-col cols=12>
+        <b-card class="tour" v-on:click="viewTour(tour)">
+          {{ tour.text }}
+        </b-card>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResultSearchTour',
+  data() {
+    return {
+      tours: [
+        { text: 'Barcelona' },
+        { text: 'Paris' },
+        { text: 'New York' },
+        { text: 'Ciudad de Mexico' },
+        { text: 'Londres' }
+      ]
+    }
+  },
+  methods: {
+    viewTour: function (tour) {
+      alert('Viendo el tour de ' + tour.text)
+    }
+  }
+}
+
+</script>
+<style scoped>
+
+.tour{
+  margin-top: 10px;
+}
+</style>
