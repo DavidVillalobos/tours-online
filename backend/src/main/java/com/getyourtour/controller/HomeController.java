b952b517734426e6e975f8258cee62acb1f46086
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-04-02 20:08:35 -0600

    rename home controller

--- backend/src/main/java/com/getyourtour/controller/HomeController.java
+++ backend/src/main/java/com/getyourtour/controller/HomeController.java
@@ -9,16 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class HomeController {
     
     @GetMapping("/")
     public String index() {
-        return "RestFul Api Get Your Tours Backend";
+        return "RestFul Api of App GetYourTours";
     }
 
     @GetMapping("/about")
     public String about() {
-        return "Get Your Tours Backend :D \n Build in Java with gradle and spring boot";
+        return "Get Your Tours :D \n Backend Build with Java, Spring boot and Gradle";
+    }
+
+    @GetMapping("/author")
+    public String author() {
+        return "{\"author\":\"Luis David Villalobos Gonzalez\",\"github\":\"https://github.com/DavidVillalobos\"}";
     }
  
 }
