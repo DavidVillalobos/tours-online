5694f80189e50cd49b14c2a9b2425a3dec5c244e
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-04-04 02:55:49 -0600

    update package and plugins

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueSession from 'vue-session'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueSession)
 
 Vue.config.productionTip = false
 
