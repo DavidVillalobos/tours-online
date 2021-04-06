1af4ea3e4653c9f41ba73db28f8b157eee29e269
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-04-06 04:59:03 -0600

    Update vue.config.js
    Add multi page

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,4 +4,27 @@ module.exports = {
     https: false,
     hotOnly: false,
   },
+  pages: {
+    index: {
+      entry: 'src/index/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Get Your Tour',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    tour: {
+      entry: 'src/tour/main.js',
+      template: 'public/tour.html',
+      filename: 'tour.html',
+      title: 'Get Your Tour/Tour',
+      chunks: ['chunk-vendors', 'chunk-common', 'tour']
+    },
+    shop: {
+      entry: 'src/shop/main.js',
+      template: 'public/shop.html',
+      filename: 'shop.html',
+      title: 'Get Your Tour/Shop',
+      chunks: ['chunk-vendors', 'chunk-common', 'shop']
+    }
+  }
 }
