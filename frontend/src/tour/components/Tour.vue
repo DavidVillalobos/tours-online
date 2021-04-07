abbab683bfba8205081ef64bc55f687243ec09e5
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-04-07 05:25:52 -0600

    Update Tour.vue
    Show images in carrusel from data in json
    Show each data from json

--- frontend/src/tour/components/Tour.vue
+++ frontend/src/tour/components/Tour.vue
@@ -1,78 +1,129 @@
 <template>
   <div>
-    <b-card bg-variant="dark">
-      <b-card bg-variant="light">
-            <b-row class="text-left"> 
-            <h5>
-              {{tour.city.name}}: {{tour.name}}
-            </h5>
-            </b-row> 
-            <b-row class="text-left"> 
-              <b-col cols=3>
-                <b-form-rating v-model="tour.rating" size="sm" variant="warning" show-value-max readonly show-value no-border></b-form-rating>
-              </b-col>
-              <b-col>
-                <b-badge variant="primary">{{ tour.reviews }} Opiniones </b-badge> 
-              </b-col>
-            </b-row>
-            <b-row class="justify-content-md-center">
-              <b-col cols=10>
-                  <b-carousel
-                    id="carousel-1"
-                    :interval="3000"
-                    controls
-                    indicators
-                    img-width="1024"
-                    img-height="480"
-                  >
-                    <b-carousel-slide id="image" img-src="http://localhost:8001/only-image?id=5">
-                    </b-carousel-slide>
-                    <b-carousel-slide id="image" img-src="http://localhost:8001/only-image?id=6">
-                    </b-carousel-slide >
-                    <b-carousel-slide id="image" img-src="http://localhost:8001/only-image?id=7">
-                    </b-carousel-slide>
-                    <b-carousel-slide id="image" img-src="http://localhost:8001/only-image?id=1">
-                    </b-carousel-slide>
-                  </b-carousel>
-              </b-col>
-            </b-row>
-            <b-row class="justify-content-md-center">
-              <b-col cols=6>
-                {{tour.description}}
-              </b-col>
-              <b-col cols=4>
-                <b-card>
-                  <b-row>
-                  <b-col cols=5>
-                  Desde
-                  <h4><strong>{{tour.price}}$ </strong> </h4> 
-                  por persona
-                  </b-col>
-                  <b-col cols=7>
-                  <b-button pill block variant="primary" >Reserva ya</b-button>
-                  </b-col>
-                  </b-row>
-                </b-card>
-              </b-col>
-            </b-row>
-            <b-row>
-            <h4><strong>Informacion general</strong></h4>
+    <b-card class="opacity" bg-variant="dark">
+      <b-card class="opacity" bg-variant="light">
+        <b-row class="justify-content-md-center"> 
+          <b-col cols=11>
+            <h2>              
+                {{tour.city.name}}: {{tour.name}}
+            </h2>
+          </b-col>
+        </b-row> 
+        <b-row class="justify-content-md-begin"> 
+          <b-col cols=1>
+          </b-col>
+          <b-col cols=3>
+            <b-form-rating v-model="tour.rating" size="sm" variant="warning" show-value-max readonly show-value no-border></b-form-rating>
+          </b-col>
+          <b-col cols=4>
+            <b-badge variant="primary">{{ tour.reviews }} Opiniones </b-badge> 
+          </b-col>
+          <b-col cols=3>
+            <template v-if="this.$session.exists() && this.$session.get('user')">
+              <template v-if="tour.liked">
+                <b-button block variant="light" @click="removeLike(tour)">
+                  <b-icon-heart-fill class="like-button" variant="danger"></b-icon-heart-fill> 
+                  Eliminar de favoritos
+                </b-button>
+              </template>
+              <template v-else>
+                <b-button block variant="light" @click="addLike(tour)">
+                  <b-icon-heart-fill class="like-button" variant="secondary"></b-icon-heart-fill> 
+                  Añadir a favoritos
+                </b-button>
+              </template>
+            </template>
+          </b-col>
+        </b-row>
+        <b-row class="justify-content-md-center">
+          <b-carousel style="background-color: #4B5964"
+              :interval="3000"
+              controls
+              indicators
+              img-width="1800"
+              img-height="700"
+              label-goto-slide = "A"
+            >
+            <div v-for="(image, idx) in tour.images" :key="idx">
+              <b-carousel-slide img-blank >
+                <b-img :src="'data:image/jpeg;base64,'+image.photo"  fluid alt="Main image Tour"></b-img>
+              </b-carousel-slide>
+            </div>
+          </b-carousel>
+        </b-row>
+        <b-row class="justify-content-md-center mt-3">
+          <b-col cols=6>
+            <h3><strong>Informacion general</strong></h3>
+            Descripcion: {{tour.description}} <br>
             <b-icon-clock-history></b-icon-clock-history>
-                Duración : {{ tour.duration }} 
-            </b-row>
-      </b-card>
-    </b-card>
-    <b-card bg-variant="dark">
-      <template v-if="tour.comments != 0">
-        <b-row v-for="(comment, idx) in tour.comments" :key="idx">
-          <b-col cols=12>
-            <b-card class="tour">
-              <b-form-rating v-model="comment.rating" size="sm" variant="warning" show-value-max readonly show-value no-border></b-form-rating>
-              {{comment.description}}
+            Duración : {{ tour.duration }} 
+            <br>
+            <b-icon-list-check></b-icon-list-check>
+            Categoria : {{ tour.category }} <br>
+            <b-icon-calendar></b-icon-calendar>
+            Fecha: {{ new Date(tour.date).toLocaleString().split(' ')[0]}} 
+            <br>
+            <b-icon-people-fill></b-icon-people-fill>
+            Cupos: {{ tour.quota }} <br>
+            <b-icon-check-circle></b-icon-check-circle>
+            Incluye: {{ tour.includes }} <br>
+            <b-icon-x-circle></b-icon-x-circle>
+            No incluye: {{ tour.notIncludes }} <br>
+          </b-col>
+          <b-col cols=1>
+          </b-col>
+          <b-col cols=4 align-self="center">
+            <b-card>
+              <b-row class="justify-content-md-center">
+                Desde                    
+                <b-badge variant="light"> <h5>${{tour.price}}</h5></b-badge>
+                por persona
+              </b-row>
+              <b-row>
+                <b-button pill block variant="primary" >Reserva ya</b-button>
+              </b-row>
             </b-card>
           </b-col>
         </b-row>
-      </template>
+      </b-card>
+    </b-card>
+    <b-card bg-variant="dark" class="opacity mt-4">
+      <b-row v-if="tour.comments !== 0">
+        <b-col cols=12 class="text-left">
+            <div class=" text-dark">
+              <h4>
+                <b-badge variant="primary">{{ tour.reviews }} Comentarios </b-badge> 
+              </h4>
+            </div>
+        </b-col>
+      </b-row>
+      <b-card class="mt-2" v-for="(comment, idx) in tour.comments" :key="idx">
+        <b-row class="justify-content-md-begin">
+          <b-col cols=2>
+            <b-form-rating v-model="comment.rating" variant="warning" readonly no-border></b-form-rating>
+          </b-col>
+        </b-row>
+        <b-row class="justify-content-md-center">
+          <b-col>
+            {{comment.description}}
+          </b-col>
+        </b-row>
+         <b-row class="justify-content-md-begin">
+          <b-col cols=1 align-self="center">
+            <b-avatar size="3em" variant="info">
+              <h5>{{ comment.user.name[0]}}{{comment.user.lastName[0] }} </h5>
+            </b-avatar>
+          </b-col>
+          <b-col cols=4 align-self="center">
+            <b-row>
+              <em>Valoración de </em>
+            </b-row> 
+            <b-row>
+              <strong>{{ comment.user.name }}  {{ comment.user.lastName }} - {{ comment.user.country.name }} </strong> 
+            </b-row>
+          </b-col>
+        </b-row>
+      </b-card>
     </b-card>
   </div>
 </template>
@@ -93,13 +144,58 @@ export default {
       alertvariant: "",
       secShowAlert: 5
     }
+
   },
   methods: {
     countDownChanged(dismissCountDown) {
         this.showAlert = dismissCountDown
     },
-    hexToBase64(str) {
-      return 'data:image/jpeg;base64,' + str;
+    async addLike(tour){
+      if(this.$session.exists() && this.$session.get('user')){
+        var Id_User = this.$session.get('user').id;
+        try {
+          const response = await fetch('http://localhost:8001/like/tour/user', {
+            method: 'POST',
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            id: 0,
+            tour: { id : tour.id },
+            user: { id : Id_User }
+          })
+        });
+        console.log(await response.json());
+        tour.liked = true;
+        } catch (err) {
+          this.messageAlert = "El servidor no responde";
+          this.alertvariant = "danger";
+          this.showAlert = this.secShowAlert; 
+        }
+      }
+    },
+    async removeLike(tour){
+      if(this.$session.exists() && this.$session.get('user')){
+        var Id_User = this.$session.get('user').id;
+        try {
+          const response = await fetch('http://localhost:8001/like/tour/user', {
+          method: 'DELETE',
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            tour: { id : tour.id },
+            user: { id : Id_User }
+          })
+        });
+        console.log(await response.json());
+        tour.liked = false;
+        } catch (err) {
+          this.messageAlert = "El servidor no responde";
+          this.alertvariant = "danger";
+          this.showAlert = this.secShowAlert;
+        }
+      }
     }
   }
 }
@@ -111,21 +207,8 @@ export default {
   margin-top: 5px;
 }
 
-.tour{
-  margin-top: 8px;
-}
-
-.tour:hover{
-  background-color: rgba(255, 255, 255, 0.926)
-}
-
-.like-button{
-  position:absolute; 
-  z-index:1;
-  width: 20%;
-  height: 20%;
-  margin-top: 1%;
-  margin-left: -16%;
+.opacity{
+  opacity: 0.95;
 }
 
 .alert-content{
@@ -134,12 +217,5 @@ export default {
   margin-top: 1%;
 }
 
-#image{
-  min-width: 100px;
-  min-height: 100px;
-  max-height: 500px;
-  max-width: 900px;
-  width: auto;
-  height: auto;
-}
+
 </style>
