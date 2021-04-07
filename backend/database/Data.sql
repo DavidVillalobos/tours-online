847530753b755e03ca0c834598aba994d0546c31
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-04-07 05:24:25 -0600

    Update Data.sql

--- backend/database/Data.sql
+++ backend/database/Data.sql
@@ -27,15 +27,17 @@ INSERT INTO [User](Id_Country, Email, Password, Name, LastName, Identification, 
 VALUES(1, '2', '2', 'Andres', 'Gonzalez', '222222222', '12-28-2004', 0)
 INSERT INTO [User](Id_Country, Email, Password, Name, LastName, Identification, Birthday, Admin) 
 VALUES(1, '3', '3', 'Giancarlo', 'Alvarado', '333333333', '01-01-1998', 0)
+INSERT INTO [User](Id_Country, Email, Password, Name, LastName, Identification, Birthday, Admin) 
+VALUES(1, '4', '4', 'Maria', 'Arias', '4444444444', '01-01-2000', 0)
 -- Insert of Tour
 INSERT INTO [Tour](Id_City, Name, Category, Description, StartDate, Quota, 
 Reviews, Duration, Price, Rating, Includes, NotIncludes) 
 VALUES (1, 'Visita Museo Nacional', 'Museo', 'Tour guiado por el museo nacional de Costa Rica',
-'04-29-2021', 20, 0, '02:00', 10.5, 4, 'Almuerzo en el Museo', 'Transporte al lugar');
+'04-29-2021', 20, 0, '02:00', 10.5, 0, 'Almuerzo en el Museo', 'Transporte al lugar');
 INSERT INTO [Tour](Id_City, Name, Category, Description, StartDate, Quota, 
 Reviews, Duration, Price, Rating, Includes, NotIncludes) 
 VALUES (9, 'Visita al Museo de Louvre', 'Museo', 'Tour guiado con un historiador atraves del museo de louvre en paris',
-'05-14-2021', 18, 0, '03:45', 15.5, 4, 'Transporte al lugar y fotos en la plaza', 'Alimentacion');
+'05-14-2021', 18, 0, '03:45', 15.5, 0, 'Transporte al lugar y fotos en la plaza', 'Alimentacion');
 
 -- Insert of Images
 -- Museo nacional
@@ -54,6 +56,7 @@ INSERT INTO Image_Tour(Id_Tour, Photo, MainPhoto) VALUES (2,
 (SELECT * FROM OPENROWSET(BULK N'C:\img\museolouvre\B.jpg', SINGLE_BLOB) as T1), 0);
 INSERT INTO Image_Tour(Id_Tour, Photo, MainPhoto) VALUES (2, 
 (SELECT * FROM OPENROWSET(BULK N'C:\img\museolouvre\C.jpg', SINGLE_BLOB) as T1), 0);
+
 -- Insert of Comments
 -- Museo Nacional
 INSERT INTO Comment_Tour(Id_Tour, Id_User, Rating, Description) VALUES (1, 1, 5, 'Buen lugar para visitar en familia');
@@ -64,6 +67,7 @@ INSERT INTO Comment_Tour(Id_Tour, Id_User, Rating, Description) VALUES (2, 3, 5,
 INSERT INTO Comment_Tour(Id_Tour, Id_User, Rating, Description) VALUES (2, 2, 3, 'La entrada es algo costosa');
 -- Insert of Likes
 INSERT INTO Like_Tour(Id_Tour, Id_User) VALUES (1, 1);
+
 -- Insert of Reservation ...
 -- Insert of DetailReservation ... 
 
