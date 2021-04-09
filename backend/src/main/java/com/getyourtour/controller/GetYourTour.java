52a30ac94a97718fa38c5f7bd1308b8e15569806
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-04-09 16:24:31 -0600

    Fix bug with comma in float

--- backend/src/main/java/com/getyourtour/controller/GetYourTour.java
+++ backend/src/main/java/com/getyourtour/controller/GetYourTour.java
@@ -9,10 +9,13 @@ package com.getyourtour.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class GetYourTour {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         SpringApplication.run(GetYourTour.class, args);
     }
 
