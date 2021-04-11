2cc689b711f58fbcf292fa7f0577f456ba16a6b9
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-04-11 02:01:32 -0600

    Add overlay to cart shopping

--- frontend/src/shop/components/CartShopping.vue
+++ frontend/src/shop/components/CartShopping.vue
@@ -1,81 +1,89 @@
 <template>
   <div>
-     <b-alert 
-        dismissible
-        :variant="alertvariant"
-        fade
-        :show="showAlert"
-        @dismissed="showAlert=false"
-        @dismiss-count-down="countDownChanged">
-        {{ messageAlert }}
-      </b-alert>
-    <b-card id="cart-shopping" bg-variant="dark">
-      <b-card id="cart-content" bg-variant="light">
-        <template v-if="items.length != 0">
-           <b-row class="mt-4 text-center">
-            <b-col cols=10>
-              <b-icon-cart-check-fill variant="primary" scale="4" shift-v="20"></b-icon-cart-check-fill>
-              <h3> Carrito de Compras </h3>
-            </b-col>
-             <b-col cols=2>
-               <b-button variant="primary" @click="returnIndex"> Continuar agregando</b-button>
-            </b-col>
-          </b-row>
-          <b-table hover responsive :items="items" :fields="fields" @row-clicked="viewTour"></b-table>
-          <b-table-simple small caption-top responsive>
-            <b-tbody class="text-center">
-              <b-tr>
-                <b-td><h6>Sub-Total</h6></b-td>
-                <b-td><h6>{{getSubTotal}}</h6></b-td>
-              </b-tr>
-              <b-tr>
-                <b-td><h6>Comisión del {{percentCommission * 100}}%</h6></b-td>
-                <b-td><h6>{{getCommission}}</h6></b-td>
-              </b-tr>
-              <b-tr>
-                <b-td><h6>Total</h6></b-td>
-                <b-td><h6>{{getTotal}}</h6></b-td>
-              </b-tr>
-              <b-tr class="text-center">
-                <b-td>
-                  <b-button variant="danger" @click="cleanCart">
-                    <b-icon-trash-fill></b-icon-trash-fill>
-                    Limpiar carrito
-                  </b-button>
-                </b-td>
-                <b-td>
-                  <b-button variant="success" @click="confirmPurchase">
-                    <b-icon-credit-card-fill></b-icon-credit-card-fill>
-                    Confirmar compra
-                  </b-button>
-                </b-td>
-               
-              </b-tr>
-            </b-tbody>
-          </b-table-simple>
-        </template>
-        <template v-else>
-          <b-row align-h="center" align-v="center" class="mt-3 text-center">
-            <b-col cols=4>
-              <b-icon-cart-x-fill variant="danger" scale="4"></b-icon-cart-x-fill>
-            </b-col>
-            <b-col cols=4>
-              <h4> Tu carrito de compra esta vacio </h4>
-            </b-col>
-          </b-row>
-          <b-row align-h="center" align-v="center" class="mt-5 text-center">
-            <b-col cols=4>
-              <h5>Agrega tours al carrito!</h5> 
-            </b-col>
-            <b-col cols=4>
-              <b-button variant="primary" @click="returnIndex">
-                Volver a la pagina principal
-              </b-button>
-            </b-col>
-          </b-row>
-        </template>
+    <b-alert 
+      dismissible
+      :variant="alertvariant"
+      fade
+      :show="showAlert"
+      @dismissed="showAlert=false"
+      @dismiss-count-down="countDownChanged">
+      {{ messageAlert }}
+    </b-alert>
+    <b-overlay :show="showOverlay">
+      <b-card id="cart-shopping" bg-variant="dark">
+        <b-card id="cart-content" bg-variant="light">
+          <template v-if="items.length != 0">
+            <b-row class="mt-4 text-center">
+              <b-col cols=10>
+                <b-icon-cart-check-fill variant="primary" scale="4" shift-v="20"></b-icon-cart-check-fill>
+                <h3> Carrito de Compras </h3>
+              </b-col>
+              <b-col cols=2>
+                <b-button variant="primary" @click="returnIndex"> Continuar agregando</b-button>
+              </b-col>
+            </b-row>
+            <b-table hover responsive :items="items" :fields="fields" @row-clicked="viewTour"></b-table>
+            <b-table-simple small caption-top responsive>
+              <b-tbody class="text-center">
+                <b-tr>
+                  <b-td><h6>Sub-Total</h6></b-td>
+                  <b-td><h6>{{getSubTotal}}</h6></b-td>
+                </b-tr>
+                <b-tr>
+                  <b-td><h6>Comisión del {{percentCommission * 100}}%</h6></b-td>
+                  <b-td><h6>{{getCommission}}</h6></b-td>
+                </b-tr>
+                <b-tr>
+                  <b-td><h6>Total</h6></b-td>
+                  <b-td><h6>{{getTotal}}</h6></b-td>
+                </b-tr>
+                <b-tr class="text-center">
+                  <b-td>
+                    <b-button variant="danger" @click="cleanCart">
+                      <b-icon-trash-fill></b-icon-trash-fill>
+                      Limpiar carrito
+                    </b-button>
+                  </b-td>
+                  <b-td>
+                    <b-button variant="success" @click="confirmPurchase">
+                      <b-icon-credit-card-fill></b-icon-credit-card-fill>
+                      Confirmar compra
+                    </b-button>
+                  </b-td>
+                
+                </b-tr>
+              </b-tbody>
+            </b-table-simple>
+          </template>
+          <template v-else>
+            <b-row align-h="center" align-v="center" class="mt-3 text-center">
+              <b-col cols=4>
+                <b-icon-cart-check-fill variant="success" scale="4"></b-icon-cart-check-fill>
+              </b-col>
+              <b-col cols=4>
+                <h4> Tu carrito ahora está vacio </h4>
+              </b-col>
+            </b-row>
+            <b-row align-h="center" align-v="center" class="mt-5 text-center">
+              <b-col cols=4>
+                <h5>Gracias por comprar con GetYourTour!</h5> 
+              </b-col>
+              <b-col cols=4>
+                <b-button variant="primary" @click="returnIndex">
+                  Volver a la pagina principal
+                </b-button>
+              </b-col>
+            </b-row>
+          </template>
+        </b-card>
       </b-card>
-    </b-card>
+    <template #overlay>
+        <div class="text-center">
+          <b-icon icon="stopwatch" font-scale="5" animation="cylon"></b-icon>
+          <p id="cancel-label">Realizando reservacion...</p>
+        </div>
+      </template>
+    </b-overlay>
   </div>
 </template>
 
@@ -101,7 +109,8 @@ export default {
       messageAlert: "",
       showAlert: 0,
       alertvariant: "",
-      secShowAlert: 5
+      secShowAlert: 5,
+      showOverlay: false
     }
   },
   computed: {
@@ -140,6 +149,7 @@ export default {
     },
     async confirmPurchase(){
       if(this.$session.has('user')){
+        this.showOverlay = true;
         try {
         const response = await fetch('http://localhost:8001/reservation', {
           method: 'POST',
@@ -147,22 +157,23 @@ export default {
             "Content-Type": "application/json"
           },
           body: JSON.stringify({
-            user: {
-              id: this.$session.get('user').id
-            },
+            user: this.$session.get('user'),
+            commission: this.getCommission,
             subTotal: this.getSubTotal,
             total: this.getTotal,
             details: this.items
           })
         });
         console.log(await response.json());
-        this.messageAlert = "Reservacion realizada con exito, correo con el detalle enviado";
+        this.messageAlert = "Reservacion realizada con exito, correo con el detalle enviado, vaciando carrito...";
         this.alertvariant = "success";
         this.showAlert = this.secShowAlert; 
+        setTimeout(()=> { this.cleanCart() }, 4500);
         } catch (err) {
           this.messageAlert = "El servidor no responde";
           this.alertvariant = "danger";
         }
+        this.showOverlay = false;
         this.showAlert = this.secShowAlert; 
       }
     },
