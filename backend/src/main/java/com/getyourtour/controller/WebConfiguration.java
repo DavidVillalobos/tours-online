ca38f15f98fdb799a1ad3f06c40fa0e1a3a98bdd
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-05-11 00:54:26 -0600

    Remove restriccion for CORS

--- backend/src/main/java/com/getyourtour/controller/WebConfiguration.java
+++ backend/src/main/java/com/getyourtour/controller/WebConfiguration.java
@@ -25,7 +25,6 @@ public class WebConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8002")
                         .allowedMethods("GET","POST","PUT","DELETE");
             }
         };
